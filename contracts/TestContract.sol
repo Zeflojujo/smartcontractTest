ac1b9b35fc36d2617d9c69f5d280f4ba84eb66aa
josephat <zeflojujo52@gmail.com>
2023-12-10 13:32:39 -0800

    adding contract test

---
+++ contracts/TestContract.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract TestContract {
+
+    //variable declaration
+    int256 intergerNumber;
+    uint256 age;
+    string public name;
+    bool private isNumber;
+    bytes1 internal character;
+    address public myAddr;
+
+    string[] nameArray;
+    mapping(string => uint256) public keyValueArray;
+
+
+}
